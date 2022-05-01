bd31ee564867cd1652d0481eae7bd2b3bf2ea1fc
Victor Manoel <victor1721556@gmail.com>
2022-05-01 12:11:17 -0300

    testing

--- Archives/main.py
+++ Archives/main.py
@@ -1,7 +1,8 @@
 import sys
-from PyQt6.QtGui import *
-from PyQt6.QtCore import *
-from PyQt6.QtWidgets import *
+from PySide2.QtGui import *
+from PySide2.QtCore import *
+from PySide2.QtWidgets import *
+import PyQt5
 
 class Window(QMainWindow):
     def __init__(self):
@@ -50,4 +51,4 @@ if __name__ == '__main__':
     mainwindow = QApplication(sys.argv)
     window = Window()
     window.show()
-    sys.exit(mainwindow.exec())
+    sys.exit(mainwindow.exec_())
