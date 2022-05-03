57ddb8f52f2b9233044e45a8fd53100bcb3a9369
Victor Manoel <victor1721556@gmail.com>
2022-05-03 00:48:05 -0300

    update 0.1

--- Archives/main.py
+++ Archives/main.py
@@ -2,51 +2,86 @@ import sys
 from PySide2.QtGui import *
 from PySide2.QtCore import *
 from PySide2.QtWidgets import *
-import PyQt5
 
 class Window(QMainWindow):
     def __init__(self):
         super(Window, self).__init__()
         self.definitions_window()
-        self.labels_and_buttons_login()
 
     def definitions_window(self):
         self.setMinimumSize(1080, 720)
         self.setWindowTitle('TrazPRO')
-        self.background = """
+
+        self.setStyleSheet('''
             QMainWindow{
                 background-color: black;
-                background-image: url(C:/Users/Victor/Downloads/testmain.png);
+                background-image: url(../Images/home_page/background_frame_one.png);
                 background-repeat: no-repeat; 
                 background-position: center;}
-        """
-
-        self.setStyleSheet(self.background)
+        ''')
         
         self.first_window = QWidget(self)
         self.setCentralWidget(self.first_window)
         self.layout = QGridLayout()
-        
         self.first_window.setLayout(self.layout)
 
-    def labels_and_buttons_login(self):
-        self.menu_frame = QFrame(self)
-        self.menu_frame.setStyleSheet('QFrame{background-color: black;}')
-        self.menu_frame.setMinimumSize(1080,50)
-        self.menu_frame.setMaximumSize(1080, 50)
+        self.menu_window_home()
+        self.labels_home()
+        self.buttons_home()
 
-        self.button = QPushButton('teste', self.first_window)
-        self.button.setStyleSheet('QPushButton{background-color: none; color: white; background-color: none;}')
-        self.button.setMinimumSize(70, 50)
-        self.button.setMaximumSize(70, 50)
+    def menu_window_home(self):
+        self.home_menu_window = QFrame(self)
+        self.home_menu_window.setMinimumSize(1080, 50)
+        self.home_menu_window.setMaximumSize(1920, 50)
+        self.home_menu_window.setStyleSheet('''
+            QFrame{
+                background-color: #000000;
+                border-radius: 12px;
+            }
+        ''')
+        self.layout.addWidget(self.home_menu_window, 0, 0)
 
-        self.label = QLabel('title', self.menu_frame)
-        self.label.setPixmap(QPixmap(
-        'C:/Users/Victor/Pictures/Imgs_project/logo_white.png'))
-        
+    def labels_home(self):
+        self.title_text = QLabel('ECONOMIZE TEMPO E DINHEIRO\nCOM GESTÃO DE FRETES', self)
+        self.title_text.setStyleSheet('''
+            QLabel{
+                background: none;
+                color: #ffffff;
+                font: bold 'Helvetica';
+                font-size: 35px;
+            }
+        ''')
+        self.apresentation_text = QLabel( 
+            'Faça a cotação das suas encomendas para\nconferir todas as taxas e\nprazos de entrega',
+            self)
+        self.apresentation_text.setStyleSheet('''
+            QLabel{
+                background: none;
+                color: #ffffff;
+                font: 'Helvetica';
+                font-size: 25px;
+            }
+        ''')
 
-        self.layout.addWidget(self.menu_frame, 0, 0)
-        self.layout.addWidget(self.button, 2, 2)
+        self.layout.addWidget(self.title_text, 1, 0)
+        self.layout.addWidget(self.apresentation_text, 2, 0)
+
+    def buttons_home(self):
+        self.register_home = QPushButton('Cadastre-se', self)
+        self.register_home.setMinimumSize(150, 30)
+        self.register_home.setMaximumSize(150, 30)
+        self.register_home.setStyleSheet('''
+            QPushButton{
+                background-color: #ffffff;
+                color: #000000;
+                border-radius: 4px;
+                font: bold 'Verdana';
+                font-size: 15px;
+            }
+        ''')
+
+        self.layout.addWidget(self.register_home, 3, 0)
+
 if __name__ == '__main__':
     mainwindow = QApplication(sys.argv)
     window = Window()
