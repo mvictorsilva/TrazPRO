a952f3ba5ec86370e0ac4835665480e021d417d0
Victor Manoel <victor1721556@gmail.com>
2022-06-09 07:34:55 -0300

    Update

--- Archives/BackEnd/apitest.py
+++ Archives/BackEnd/apitest.py
@@ -1,21 +1,20 @@
 import requests
-
+from pprint import pprint
+import json
 
 url = "https://api.linketrack.com/track/json?" \
-      "user=teste&token=1abcd00b2731640e886fb41a8a9671ad1434c599dbaa0a0de9a5aa619f29a83f&" \
+      "user=teste&" \
+      "token=1abcd00b2731640e886fb41a8a9671ad1434c599dbaa0a0de9a5aa619f29a83f&" \
       "codigo=QJ331080523BR"
+      
 
-payload = {}
-headers = {}
+request = requests.get(url)
+reques = request.json()
 
-request = requests.request("GET", url, headers=headers, data=payload)
-request = request.json()
+# pprint(reques)
 
-# print(request['eventos'][2])
+reque = reques['eventos']
 
-reques = request['eventos']
-
-for r in reques:
-#     print(r)
-    for i in r.values():
-        print(i)
+for pontos in reque:
+     for valores in pontos:
+         print(pontos[valores])    
